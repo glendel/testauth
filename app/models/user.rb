fbea58c3189edec99c5c3a9841b1b819def7b8e7
william huertas <williamhuertas1015@gmail.com>
2013-04-04 02:03:38 -0500

    commit 04-04-13

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ActiveRecord::Base
+  has_many :authentications
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # Setup accessor
+  attr_accessor :loginV
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :login, :email, :password, :password_confirmation, :remember_me, :loginV
+  # attr_accessible :title, :body
+ #=============================================================
+ # self.find_first_by_auth_conditions
+ #=============================================================
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+     if login = conditions.delete(:loginV)
+        return( where(conditions).where(["lower(login) = :value or lower(email) = :value", { :value => login.downcase }]).first )
+     else
+      return( where(conditions).first )
+     end
+end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testauth::Application.config.secret_token = '57b0ae7920d128b8234993347d89b83420a5ecbf1564d94cf7f26e7a29d864dac87a7e6edfb7e52f10930b25d8caefd73cc45119c214c72b88f934271b05a7ac'
